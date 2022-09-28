e17b12f08d0121fdad62f37284738651e6c5b0ae
Marvin <marvinwanzuita@gmail.com>
2022-09-28 15:06:50 -0300

    Exercicio 1016

---
+++ _1016_/_1016_Main.java
@@ -0,0 +1,18 @@
+package beecrowd.iniciante._1016_;
+
+import java.util.Scanner;
+
+public class _1016_Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int distancia = sc.nextInt();
+
+        distancia *= 2;
+
+        System.out.println(distancia + " minutos");
+
+    }
+}
