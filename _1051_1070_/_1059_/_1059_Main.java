d597e9dd73d057e3b848654a1b648b3f06f7d80c
Marvin <marvinwanzuita@gmail.com>
2022-11-08 10:39:03 -0300

    Exercicio 1059

---
+++ _1051_1070_/_1059_/_1059_Main.java
@@ -0,0 +1,15 @@
+package beecrowd.iniciante._1051_1070_._1059_;
+
+public class _1059_Main {
+
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 100; i++) {
+            if ((i % 2) == 0){
+                System.out.println(i);
+            }
+        }
+
+
+    }
+}
