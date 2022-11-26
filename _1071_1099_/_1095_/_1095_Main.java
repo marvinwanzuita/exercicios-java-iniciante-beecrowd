3953396b272ea5776ed9e0f030f2f4704041c8ac
Marvin <marvinwanzuita@gmail.com>
2022-11-26 15:25:33 -0300

    Exercicio 1095

---
+++ _1071_1099_/_1095_/_1095_Main.java
@@ -0,0 +1,16 @@
+package beecrowd.iniciante._1071_1099_._1095_;
+
+public class _1095_Main {
+
+    public static void main(String[] args) {
+
+        int i = 1;
+
+        for (int j = 60; j >= 0 ; j -= 5) {
+            System.out.println("I=" + i + " J=" + j );
+            i += 3;
+        }
+
+    }
+
+}
