ef46035aab652b7818a1323d84bd076c1457586d
Marvin <marvinwanzuita@gmail.com>
2022-12-18 10:11:07 -0300

    Exercicio 1143

---
+++ _1141_1160_/_1143_/_1143_Main.java
@@ -0,0 +1,19 @@
+package beecrowd.iniciante._1141_1160_._1143_;
+
+import java.util.Scanner;
+
+public class _1143_Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int quantidadeSaidas= sc.nextInt();
+
+        for (int i = 1; i <= quantidadeSaidas; i++) {
+            System.out.println(i + " " + (i * i) + " " + (i * i * i));
+        }
+
+    }
+
+}
