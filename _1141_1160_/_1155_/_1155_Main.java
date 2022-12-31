e3ab31c95263fe41bf6685d567fc17c97aa8a899
Marvin <marvinwanzuita@gmail.com>
2022-12-31 15:47:53 -0300

    Exercicio 1155

---
+++ _1141_1160_/_1155_/_1155_Main.java
@@ -0,0 +1,19 @@
+package beecrowd.iniciante._1141_1160_._1155_;
+
+public class _1155_Main {
+
+    public static void main(String[] args) {
+
+        double S = 1;
+
+        for (double i = 1; i <= 99; i++) {
+
+            S += 1 / (i + 1);
+
+        }
+
+        System.out.printf("%.2f%n", S);
+
+    }
+
+}
